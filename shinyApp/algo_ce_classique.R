c79b8ad8e2dbe5b0c0db47e2f88d6c74b257c402
AQLT <alain.quartier@yahoo.fr>
2020-04-02 19:31:45 +0200

    try catch transformé en while

--- shinyApp/algo_ce_classique.R
+++ shinyApp/algo_ce_classique.R
@@ -21,61 +21,58 @@ lancer_algorithme <- function(y, n, m, N = C * m * n, maxIters = 100,
   
   
   #### début du try
-  
-  tryCatch({
+  iter <- 0
+  critere_arret <- TRUE
+  #ceils = rounds each element of X to the nearest integer greater than or equal to that element.
+  eidx = ceiling((1-rho)*N) #plus petit indice du meilleur Score.
+  while(critere_arret & (iter+1)<= maxIters){
+    iter <- iter + 1
     
-    for(iter in 1:maxIters){
-      X <- initialisation_sample(m = m, n = n, N = N,
-                                 P_hat = P_hat_liste[[iter]],
-                                 avec_remise = avec_remise)
-      
-      
-      #### Calcul du score
-      
-      scores <- apply(X, 1, score,
-                      y = y, poids_noir = poids_noir, poids_blanc = poids_blanc)
-      
-      scores_tries <- sort(scores)
-      
-      # Mise à jour de Gamma 
-      #ceils = rounds each element of X to the nearest integer greater than or equal to that element.
-      eidx = ceiling((1-rho)*N) #plus petit indice du meilleur Score.
-      gamma = scores_tries[eidx]
-      s = scores_tries[N]
-      #  meilleur_score = max(meilleur_score,  scores_tries[N]) #garder une trace du meilleur résultat
-      gammas_hat[iter] = gamma
-      s_max[iter] = s
-      # meilleur_scores[iter] = meilleur_score
-      
-      for(i in 1:n){
-        for(j in 1:m){
-          P_hat_tilde[i,j]=sum(scores>=gamma & X[,i]==j)/sum(scores>=gamma)
-        }
+    X <- initialisation_sample(m = m, n = n, N = N,
+                               P_hat = P_hat_liste[[iter]],
+                               avec_remise = avec_remise)
+    
+    #### Calcul du score
+    
+    scores <- apply(X, 1, score,
+                    y = y, poids_noir = poids_noir, poids_blanc = poids_blanc)
+    
+    scores_tries <- sort(scores)
+    
+    # Mise à jour de Gamma 
+    gamma = scores_tries[eidx]
+    s = scores_tries[N]
+    #  meilleur_score = max(meilleur_score,  scores_tries[N]) #garder une trace du meilleur résultat
+    gammas_hat[iter] = gamma
+    s_max[iter] = s
+    # meilleur_scores[iter] = meilleur_score
+    
+    for(i in 1:n){
+      for(j in 1:m){
+        P_hat_tilde[i,j]=sum(scores>=gamma & X[,i]==j)/sum(scores>=gamma)
       }
-      # Smoothing
-      if(smoothing){
-        #P_hat <- alpha * P_hat_tilde + (1-alpha)* P_hat_liste[[iter-1]]
-        P_hat <- alpha * P_hat_tilde + (1-alpha)* P_hat_liste[[iter]] 
-      } else{
-        P_hat <- P_hat_tilde
-      }
-      
-      P_hat_liste[[iter+1]] <- P_hat
-      
-      if(length(gammas_hat) > d & is.null(indice_stop)){
-        gammas_d <- gammas_hat[(length(gammas_hat)-d):length(gammas_hat)]
-        if(length(unique(gammas_d))==1){
-          indice_stop <- iter
-          if(stop_d){
-            stop("Dernière itération : ", indice_stop)  
-          }
-        }
-      }
-      
-      
+    }
+    # Smoothing
+    if(smoothing){
+      #P_hat <- alpha * P_hat_tilde + (1-alpha)* P_hat_liste[[iter-1]]
+      P_hat <- alpha * P_hat_tilde + (1-alpha)* P_hat_liste[[iter]] 
+    } else{
+      P_hat <- P_hat_tilde
     }
     
-  },error=function(e){})
+    P_hat_liste[[iter+1]] <- P_hat
+    
+    if(length(gammas_hat) > d & is.null(indice_stop)){
+      gammas_d <- gammas_hat[(length(gammas_hat)-d):length(gammas_hat)]
+      if(length(unique(gammas_d))==1){
+        indice_stop <- iter
+        if(stop_d){
+          critere_arret <- FALSE
+        }
+      }
+    }
+  }
+  
   ### fin de try
   duree <- round(as.numeric(difftime(Sys.time(), duree),units="secs"),2)
   
